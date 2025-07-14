5a5a282daf4e07a3f6fb0b0bfce8cd663c49830d
worghet <worker.forget@gmail.com>
2025-07-14 09:57:08 -0400

    added ps1 shader
    followed video by DevPoodle

--- carvibes/scripts/camera_pivot.gd
+++ carvibes/scripts/camera_pivot.gd
@@ -5,18 +5,34 @@ var direction = Vector3.FORWARD
 @export var smooth_speed = 2.5
 
 
+var rotating_w_mouse = false
+
 
 func _physics_process(delta: float) -> void:
 	var current_velocity = get_parent().get_linear_velocity()
 	current_velocity.y = 0
 	
 	
-	if current_velocity.length_squared() > 1:
+	if current_velocity.length_squared() > 1 and !rotating_w_mouse:
 		direction = lerp(direction, -current_velocity.normalized(), smooth_speed*delta)
 	
 	# set pivot to move
 	
 	global_transform.basis = get_rotation_from_direction(direction)
+
+func _unhandled_input(event: InputEvent) -> void:
+	
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)	
+	elif Input.is_action_just_pressed("escape"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)	
+#
+	#if Input.MOUSE_MODE_CAPTURED and Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
+		#rotating_w_mouse = true
+		#rotate_y(0.4*delta)
+	#else:
+		#rotating_w_mouse = false
+		
 	
 func get_rotation_from_direction(look_direction : Vector3) -> Basis:
 	look_direction = look_direction.normalized()
--- carvibes/scripts/car.gd
+++ carvibes/scripts/car.gd
@@ -3,8 +3,12 @@ extends VehicleBody3D
 var max_rpm = 500
 var max_torque = 200
 
+
+var max_wheel_turn = 0.45 # max wheel turn angle
+var steering_speed = 5 # how fast to get to the max wheel turn
+
 func _process(delta: float) -> void:
-	steering = lerp(steering, Input.get_axis("right", "left") * 0.4, 5*delta)
+	steering = lerp(steering, Input.get_axis("right", "left") * max_wheel_turn, delta * steering_speed)
 	
 	var acceleration = Input.get_axis("backward", "forward") # * 100
 
@@ -19,6 +23,20 @@ func _process(delta: float) -> void:
 	#if Input.is_action_pressed("brake"): 
 		#linear_velocity = linear_velocity.linear_interpolate(Vector3.ZERO, 5 * delta)
 
+
+func _input(event: InputEvent) -> void:
+	pass
+
+func _unhandled_input(event: InputEvent) -> void:
+	
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)	
+	elif Input.is_action_just_pressed("escape"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)	
+			#
+		#) = Input.MOUSE_MODE_CAPTURED
+		#
+		#print("move cam now")
 
 		
 	
