11801027205c05611b0761bbeabb7377da34a697
worghet <worker.forget@gmail.com>
2025-07-17 12:29:28 -0400

    remade car physics + track feature
    car handling was changed:
    
    [shift] - boost while drifting
    
    demo tracks list:
    1.Twin Peaks Theme
    2. Ocarina of Time Slowed
    3. Resonance - HOME
    
    [up-arrow] - previous track
    [down-arrow] - next track
    [p] - pause
    
    *also slightly made the map longer

--- carvibes/scripts/car.gd
+++ carvibes/scripts/car.gd
@@ -3,12 +3,19 @@ extends VehicleBody3D
 @onready var accelerometer_ui = get_parent().get_child(0).get_child(0).get_child(0)
 
 var max_rpm = 500 # max speed
-var max_torque = 200 #acceleration
+var max_torque = 500 #power
 
 var braking_speed = 3.25
 
-var max_wheel_turn = 0.45 # max wheel turn angle
-var steering_speed = 5 # how fast to get to the max wheel turn (2-3 reccomended for chill)
+var max_wheel_turn = deg_to_rad(22.5) # max wheel turn angle
+var steering_speed = 2.75 # how fast to get to the max wheel turn (2-3 reccomended for chill)
+# maybe make speed a curve
+
+var dynamic_fov_scale = 2
+
+
+
+var is_drifting = false
 
 var should_reset = false
 
@@ -17,27 +24,38 @@ func _process(delta: float) -> void:
 	# update accelerometer ui
 	
 	accelerometer_ui.text = "SPEED   //   " + str(linear_velocity.length()).pad_decimals(1)
-	steering = lerp(steering, Input.get_axis("right", "left") * max_wheel_turn, delta * steering_speed)
+	
+	
+	
+	steering = lerp(steering, Input.get_axis("left", "right") * max_wheel_turn, delta * steering_speed)
 	
 	var acceleration = Input.get_axis("backward", "forward") # * 100
 
 	var rpm = $"back-left".get_rpm()
 	
 	$"back-left".engine_force = acceleration * max_torque * (1-abs(rpm) / max_rpm) # fixed backwards thing w abs
+	#$"back-left".engine_force = $"front-left".engine_force * 2
+	
 	
 	rpm = $"back-right".get_rpm()
 	
 	$"back-right".engine_force = acceleration * max_torque * (1-abs(rpm) / max_rpm)
-	
+	#$"back-right".engine_force = $"front-right".engine_force * 2
 	
 	#dynamic fov
-	$camera_pivot/camera.fov = lerp($camera_pivot/camera.fov, 70 + (linear_velocity.length() * 2), 5*delta) # 0 for no dynamic fov
+	$camera_pivot/camera.fov = lerp($camera_pivot/camera.fov, 70 + (linear_velocity.length() * dynamic_fov_scale), 5*delta) # 0 for no dynamic fov
 	
 	# braking
 	if Input.is_action_pressed("brake"): 
 		#bug: will also keep the car floating if in the air..
 		linear_velocity = lerp(linear_velocity, Vector3.ZERO, delta * braking_speed)
-
+		
+		
+	if is_drifting:
+		#engine force is recalculated all the time
+		$"back-right".engine_force *= 5
+		$"back-left".engine_force *= 5
+	
 
 
 func _input(event: InputEvent) -> void:
@@ -45,6 +63,11 @@ func _input(event: InputEvent) -> void:
 	# activate flag
 	if Input.is_action_just_pressed("reset_car"):
 		should_reset = true
+		
+	if Input.is_action_pressed("drift"):
+		is_drifting = true
+	else:
+		is_drifting = false
 		
 		
 func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
---
+++ carvibes/scripts/demo_audio.gd
@@ -0,0 +1,76 @@
+extends AudioStreamPlayer
+
+
+@onready var twin_peaks_mp3 = load("res://demo_stuffs/Twin Peaks Intro.mp3")
+@onready var ocarina_of_time_mp3 = load("res://demo_stuffs/Ocarina of Time.mp3")
+@onready var resonance_mp3 = load("res://demo_stuffs/Resonance.mp3")
+
+@onready var track_list : Dictionary = {
+	
+	"twin_peaks.mp3": twin_peaks_mp3,
+	"ocarina_of_time.mp3" : ocarina_of_time_mp3,
+	"resonance.mp3" : resonance_mp3
+	
+}
+
+@onready var track_ui = get_parent().get_parent().get_child(0).get_child(1).get_child(0)
+
+const PREV = 0
+const NEXT = 1
+
+
+var track_index = 0
+
+func _ready() -> void:
+	
+	update_track(PREV)
+
+
+func _input(event: InputEvent) -> void:
+	
+	
+	if Input.is_action_just_pressed("previous_track"):
+
+		update_track(PREV)
+	
+	if Input.is_action_just_pressed("next_track"):
+		
+		update_track(NEXT)
+		
+	if Input.is_action_just_pressed("pause_track"):
+		stream_paused = !stream_paused
+		
+		if stream_paused:
+			track_ui.text = "<   PAUSED  :  " + track_list.keys()[track_index] + "   />"
+		else:
+			track_ui.text = "<   " + track_list.keys()[track_index] + "   />"
+	
+	
+	
+func update_track(ACTION):
+	
+	if ACTION == PREV and !track_index == 0:
+		track_index -= 1
+		
+	
+	elif ACTION == NEXT:
+		if track_index < track_list.keys().size() -1:
+			track_index += 1
+		else:
+			track_index = 0
+	
+	
+	var track_key = track_list.keys()[track_index]
+	stream = track_list.get(track_key)	
+	
+	#stream.resource_name
+	track_ui.text = "<   " + track_key + "   />"
+	play()
+	
+
+
+
+
+func _on_finished() -> void:
+	update_track(NEXT)
+	
